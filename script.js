9a60ec7a1c9246afcbaa681892be22d16b333307
Roger Nora <109190689+RogerNora@users.noreply.github.com>
2025-08-03 19:00:19 -0300

    trestando

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let A = document.querySelector("#par_A");
-console.log(A);
-function calcula (A, B, C){
+function calcularX(){
+    const A = parseFloat(document.querySelector("#par_A").value);
+    const B = parseFloat(document.querySelector("#par_B").value);
+    const C = parseFloat(document.querySelector("#par_C").value);
+
+    document.getElementById("resultado").innerHTML = "os valor de A, B e C são";
+    
 
 
 
 }
+
+
+
