e962aa23afc97a9be9b383a09b8a52f7e10870e1
Roger Nora <109190689+RogerNora@users.noreply.github.com>
2025-08-11 09:43:18 -0300

    Funcionando

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-function calcularX(){
-    const A = parseFloat(document.querySelector("#par_A").value);
-    const B = parseFloat(document.querySelector("#par_B").value);
-    const C = parseFloat(document.querySelector("#par_C").value);
+  // captura os elementos do HTML
+const A = document.getElementById("par_A");
+const B = document.getElementById("par_B");
+const C = document.getElementById("par_C");
+const botao = document.getElementById("calculaAi");
+const resultado = document.getElementById("resultado");
 
-    document.getElementById("resultado").innerHTML = "os valor de A, B e C são";
+// evento de clique
+botao.addEventListener("click", function(){
     
+// captura os valores como texto
+const valorA = A.value.trim();
+const valorB = B.value.trim();
+const valorC = C.value.trim();
 
-
-
-}
+  // verifica se os campos estão preenchidos
+if (valorA === "" || valorB === "" || valorC === "") {
+resultado.textContent = "Preencha todos os campos!";
+return;
+}  
+    
+//le os valores como numeros
+const a = Number(A.value);
+const b = Number(B.value);
+const c = Number(C.value);
+    
+    //verifica se são numeros validos
+if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+    
+    // verifica se é mesmo uma equação de segundo grau
+    if(a ===0){
+        resultado.textContent = "Para de frescura e faz na mao";
+    } else{
+        //calcula delta
+        const delta = b*b-4*a*c;
+            
+        if (delta<0){
+            resultado.textContent = "Não tem raiz real"
+        } else {
+        // tira raiz quadrada de delta
+            const raizDelta = Math.sqrt(delta);
+            //calcula as raizes 
+            const x1 = (-b+raizDelta)/(2*a);
+            const x2 = (-b-raizDelta)/(2*a);
+            //mostra o resultado 
+            resultado.textContent = `Raizes x1 = ${x1}  e x2 = ${x2}`
+        }               
+    }
+    } else {
+            //caso seja numero invalido
+            resultado.textContent = "Por favor, insira valores válidos.";
+    } 
+});
 
 
 
